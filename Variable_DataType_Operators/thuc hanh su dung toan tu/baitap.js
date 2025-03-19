e9f120ea7d54bde9331a2d3aef4a47aba4e7a7e2
HoangLong205 <hoanglong2kk5@gamil.com>
2025-03-19 17:20:52 +0700

    baitap su dung toan tu

---
+++ Variable_DataType_Operators/thuc hanh su dung toan tu/baitap.js
@@ -0,0 +1,10 @@
+var inputWidth = prompt('Enter a width: ');
+var inputHeight = prompt('Enter a height: ');
+
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+
+var area = width * height;
+
+document.write('the area is: ' + area);
+
