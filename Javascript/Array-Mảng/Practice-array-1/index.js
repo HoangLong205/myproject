0c9a59251d5c02476cd90e6a684e1fa99c0b41c6
HoangLong205 <hoanglong2kk5@gamil.com>
2025-04-09 10:11:06 +0700

    bai tap

--- Javascript/Array-Mảng/Practice-array-1/index.js
+++ Javascript/Array-Mảng/Practice-array-1/index.js
@@ -2,7 +2,7 @@ let girls = ["Lan", "Hoa", "Dao", "Huong"];
 let boys = ["Ti", "Teo", "Tuat", "Hoi"];
 let count = girls.length > boys.length ? boys.length : girls.length;
 for (var i = 0; i < count; i++) {
-    console.log('Cap ' + i + ': ' + girls[getRandomGirlIndex(girls)] + ' - ' + boys[getRandomGirlIndex(boys)]);
+    console.log('Cap ' + i + ': ' + girls[getRandomGirlIndex(girls)] + ' - ' + boys[getRandomBoyIndex(boys)]);
 }
 
 function getRandomGirlIndex(girl) {
